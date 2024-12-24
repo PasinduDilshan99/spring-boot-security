3098445c37c0660f468159f5ef5a930aaf09cd25
Pasindu Dimbulana <pasindudi@mobitel.lk>
2024-12-24 12:52:16 +0530

    add code for users in inmemory

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.spring.security.config;
 
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.FormLoginDsl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.*;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,20 +22,80 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity.csrf(customizer -> customizer.disable());
-//        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-//
-//        httpSecurity.formLogin(Customizer.withDefaults());
-//        httpSecurity.httpBasic(Customizer.withDefaults());
-//
-//        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//
-//        return httpSecurity.build();
+        httpSecurity.csrf(customizer -> customizer.disable());
+        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
 
-        Customizer<CsrfConfigurer<>>
+        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.httpBasic(Customizer.withDefaults());
 
-        httpSecurity.csrf()
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return httpSecurity.build();
+
+//        Customizer<CsrfConfigurer<HttpSecurity>> csrfConfigurerCustomizer = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+//            @Override
+//            public void customize(CsrfConfigurer<HttpSecurity> httpSecurityCsrfConfigurer) {
+//                httpSecurityCsrfConfigurer.disable();
+//            }
+//        };
+//
+////        Customizer<AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry> authConfig = new Customizer<AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry>() {
+////            @Override
+////            public void customize(AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry authorizationManagerRequestMatcherRegistry) {
+////                authorizationManagerRequestMatcherRegistry.anyRequest().authenticated();
+////            }
+////        };
+//
+//        Customizer<FormLoginConfigurer<HttpSecurity>> formLoginConfigurerCustomizer = new Customizer<>() {
+//            @Override
+//            public void customize(FormLoginConfigurer formLoginConfigurer) {
+//                formLoginConfigurer.loginPage("/login");
+//            }
+//        };
+//
+//        Customizer<HttpBasicConfigurer<HttpSecurity>> httpBasicConfigurerCustomizer = new Customizer<HttpBasicConfigurer<HttpSecurity>>() {
+//            @Override
+//            public void customize(HttpBasicConfigurer<HttpSecurity> httpSecurityHttpBasicConfigurer) {
+//                httpSecurityHttpBasicConfigurer.realmName("CustomRealm")               // Sets the realm name for the HTTP Basic authentication
+//                        .authenticationEntryPoint((request, response, authException) -> {
+//                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // Sets HTTP 401 status
+//                            response.getWriter().write("Unauthorized: Authentication required"); // Custom error message
+//                        });
+//            }
+//        };
+//
+//        Customizer<SessionManagementConfigurer<HttpSecurity>> sessionManagementConfigurerCustomizer = new Customizer<SessionManagementConfigurer<HttpSecurity>>() {
+//            @Override
+//            public void customize(SessionManagementConfigurer<HttpSecurity> httpSecuritySessionManagementConfigurer) {
+//                httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//            }
+//        };
+//
+//        httpSecurity.csrf(csrfConfigurerCustomizer);
+//        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+//        httpSecurity.formLogin(formLoginConfigurerCustomizer);
+//        httpSecurity.httpBasic(httpBasicConfigurerCustomizer);
+//        httpSecurity.sessionManagement(sessionManagementConfigurerCustomizer);
+//        return httpSecurity.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+                .username("user1")
+                .password("user1")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("user2")
+                .password("user2")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 
 }
